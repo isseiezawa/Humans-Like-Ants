f40a829fdb6b0117fffdf0c9b08b37a906293098
issei_ezawa <issemann@yahoo.co.jp>
2022-11-16 07:14:33 +0900

    Add: time zone設定、i18n gem追加

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,13 @@ module HumansLikeAnts
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    # DB Time zone
+    config.active_record.default_timezone = :local
+    # 訳文の探索場所を指示
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
