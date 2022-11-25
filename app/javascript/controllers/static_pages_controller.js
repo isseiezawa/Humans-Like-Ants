cce22f9cb58706eb21cc054085fbabe67e93e094
issei_ezawa <issemann@yahoo.co.jp>
2022-11-26 07:16:23 +0900

    Change: top_pageからstatic_pagesに変更

--- app/javascript/controllers/static_pages_controller.js
+++ app/javascript/controllers/static_pages_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="top-page"
+// Connects to data-controller="static-pages"
 export default class extends Controller {
   connect() {
   }
