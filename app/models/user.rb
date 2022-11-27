954db46b18b11bfc87e76eb83d06a08ff921eb1a
issei_ezawa <issemann@yahoo.co.jp>
2022-11-27 23:35:05 +0900

    Add: userモデルにvalidation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,16 @@
 #
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :self_introduction, length: { maximum: 160 }
+  validates :twitter_id, length: { minimum: 4, maximum: 50 }
+
+  enum gender: { unselected: 0, man: 1, woman: 2 }
+  enum role: { general: 0, admin: 1 }
 end
