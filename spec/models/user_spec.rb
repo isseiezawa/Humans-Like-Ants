6da157f74a26b462fc1e971c26454e1c7eaade1b
issei_ezawa <issemann@yahoo.co.jp>
2022-11-27 23:01:34 +0900

    Add: bundle exec annotate 実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :bigint           not null, primary key
+#  crypted_password  :string(255)
+#  email             :string(255)      not null
+#  gender            :integer          default(0), not null
+#  name              :string(255)      not null
+#  role              :integer          default(0), not null
+#  salt              :string(255)
+#  self_introduction :text(65535)
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  twitter_id        :string(255)
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
