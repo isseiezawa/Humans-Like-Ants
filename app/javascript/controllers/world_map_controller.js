b57df6f8c1c75e5483a7dfd0e7cfc5b4705a830d
issei_ezawa <issemann@yahoo.co.jp>
2022-12-16 09:41:28 +0900

    Add: 日本地図を描写。画面中央に持っていく処理追加

--- app/javascript/controllers/world_map_controller.js
+++ app/javascript/controllers/world_map_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 import * as THREE from "three"
+import { OrbitControls } from "three/OrbitControls"
 import * as d3 from "d3"
+import { transformSVGPathExposed } from "../plugins/d3-threeD"
 
 // import geoJson from "/assets/japan.geo.json"
 
@@ -10,37 +12,125 @@ export default class extends Controller {
     window.addEventListener('DOMContentLoaded', this.init())
   }
 
-  init() {
+  async init() {
     const element = document.getElementById('japan-map-container')
     // 描画範囲
     const width = element.clientWidth,
           height = element.clientHeight
     const scale = 1500
 
-    const geoJson = '/assets/japan.geo.json'
+    console.log(element)
 
-    // 要素を追加する
-    const svg = d3.select('#japan-map-container')
-      .append('svg')
-      .attr('width', width) // svg要素の横幅を指定
-      .attr('height', height) // svg要素の縦幅を指定
+    // ***** three.js setting *****
+
+    // シーンの追加
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color( 0x00ffff );
+
+    // レンダリング
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(width, height)
+    element.appendChild(renderer.domElement)
+
+    // カメラの設定
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      width / height,
+      0.1,
+      10000
+    )
+    camera.position.set(0, 0, 400)
+
+    const controls = new OrbitControls(
+      camera,
+      renderer.domElement
+    )
+
+    scene.add(camera)
+
+    // ライトの追加
+    const light = new THREE.DirectionalLight(0xffffff)
+    light.position.set(1, -4, 1).normalize();
+
+    scene.add(light)
+
+    // 環境光源
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+    scene.add(ambientLight);
+
+    // ***** d3.js geoJson to shape *****
+
+    const geoJson = '/assets/japan.geo.json'
+    let geoData = []
 
     // 投影法
-    const projection = d3.geoMercator()
-          .center([136.0, 38.0]) // 日本の中心(経度, 緯度)
-          .translate([width/2, height/2]) //画面の中央
-          .scale(scale)
+    const projection = d3.geoMercator() // メルカトル図法を描画
+                        .center([136.0, 38.0]) // 日本の中心(経度, 緯度)
+                        .translate([width/2, height/2]) //画面の中央
+                        .scale(scale)
 
-    const geoPath = d3.geoPath(projection) // svg pathデータ文字列の生成
+    //path変換関数生成
+    const geoPath = d3.geoPath(projection)
 
-    d3.json(geoJson).then((json) => {
-      svg.selectAll('path')
-        .data(json.features) // 都道府県データの格納
-        .enter()
-        .append('path')
-          .attr('d', geoPath)
-          .style('fill', 'green') // 塗りつぶし色
-          .style('stroke', 'black') // 枠線の色
+    await d3.json(geoJson).then((json) => {
+      geoData = json.features // 都道府県データの格納
     })
+
+    //geoJSON→svg path→three.js mesh変換 
+    let places = []
+    for(let i = 0; i < geoData.length; i++) {
+      const geoFeature = geoData[i]
+      const properties = geoFeature.properties
+      const feature = geoPath(geoFeature)
+
+      // svgパスをthree.jsのmeshに変換
+      const geoMesh = transformSVGPathExposed(feature) // new THREE.Shape() に変換
+
+      places.push({'data': properties, 'mesh': geoMesh})
+    }
+
+    // ***** shape to mesh *****
+
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x00ff00
+    })
+
+    const extrudeSettings = {
+      steps: 2,
+      depth: 2,
+      bevelEnabled: false,
+      bevelThickness: 1,
+      bevelSize: 1,
+      bevelOffset: 0,
+      bevelSegments: 1
+    };
+
+    const japan = new THREE.Group()
+
+    for(let j = 0; j < places.length; j++) {
+      const geometry = new THREE.ExtrudeGeometry( places[j].mesh[0], extrudeSettings ) // シェイプから押し出されたジオメトリを作成
+      const mesh = new THREE.Mesh(geometry, material)
+
+      japan.add(mesh)
+    }
+
+    scene.add(japan)
+
+    // ***** japanを中心に持ってくる処理 *****
+    const box3 = new THREE.Box3()
+    box3.setFromObject(japan)
+    const centerX = (box3.max.x - box3.min.x) / 2 + box3.min.x
+    const centerY = (box3.max.y - box3.min.y) / 2 + box3.min.y
+    const centerZ = (box3.max.z - box3.min.z) / 2 + box3.min.z
+    japan.position.set(-centerX, -centerY, -centerZ)
+
+    animate()
+
+    function animate() {
+      requestAnimationFrame(animate)
+      renderer.render( scene, camera )
+
+      controls.update()
+    }
   }
 }
