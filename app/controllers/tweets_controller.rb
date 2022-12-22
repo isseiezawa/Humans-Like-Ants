5b9b0286405c5952ead3b6ec3bdb716868e31b6f
issei_ezawa <issemann@yahoo.co.jp>
2022-12-23 08:31:40 +0900

    Add: tweets controller create destroy format json

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,27 @@
 class TweetsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def create
+    tweet = Tweet.new(tweet_params)
+
+    if tweet.save
+      render json: { status: :ok, message: t('.success'), data: tweet }
+    else
+      render json: { status: :unprocessable_entity, message: t('.failure'), data: tweet.errors }
+    end
   end
 
-  def delete
+  def destroy
+    tweet = Tweet.find(params[:id])
+
+    tweet.destroy!
+
+    render json: { status: :ok, message: t('.success') }
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:user_id, :world_id, :post)
   end
 end
