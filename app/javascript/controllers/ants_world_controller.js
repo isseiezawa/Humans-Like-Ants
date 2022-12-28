37188b8c9708a7a4baf79f126d169362517e722d
issei_ezawa <issemann@yahoo.co.jp>
2022-12-29 04:59:23 +0900

    Add: ants_world.js(three.js)のベース作成

--- app/javascript/controllers/ants_world_controller.js
+++ app/javascript/controllers/ants_world_controller.js
@@ -1,7 +1,55 @@
 import { Controller } from "@hotwired/stimulus"
+import * as THREE from "three"
 
 // Connects to data-controller="ants-world"
 export default class extends Controller {
+  static targets = ['antsWorldElement']
   connect() {
+    window.addEventListener('DOMContentLoaded', this.init())
+  }
+
+  init() {
+    const element = this.antsWorldElementTarget
+
+    const width = element.offsetWidth
+    const height = element.offsetHeight
+
+    // シーン作成
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color( 0xF0F8FF );
+
+    // カメラ作成
+    const camera = new THREE.PerspectiveCamera(75)
+
+    // レンダラー作成
+    const renderer = new THREE.WebGLRenderer()
+    element.appendChild(renderer.domElement)
+
+    // ***** 画面のリサイズ処理 *****
+
+    onResize()
+
+    window.addEventListener('resize', onResize)
+
+    function onResize() {
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(width, height)
+      camera.aspect = width / height
+      camera.updateProjectionMatrix()
+    }
+
+    // 環境光源
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
+    const directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(1, 1, 1).normalize()
+    scene.add(ambientLight, directionalLight)
+
+    animate()
+
+    function animate() {
+      requestAnimationFrame(animate)
+
+      renderer.render(scene, camera)
+    }
   }
 }
