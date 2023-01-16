80a9b1bd6ab1576da6161de95a7a049a85b2b863
issei_ezawa <issemann@yahoo.co.jp>
2023-01-17 07:41:36 +0900

    Add: email validationの追加と、submitbuttonの処理変更

--- app/javascript/controllers/validate_controller.js
+++ app/javascript/controllers/validate_controller.js
@@ -4,16 +4,21 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [
                     'input',
+                    'email',
                     'inputLength',
-                    'formButton',
-                    'error'
+                    'submitButton',
+                    'inputError',
+                    'emailError'
                   ]
   static values = {
+                    formType: String,
                     characterCount: Number
                   }
 
-  connect() {
-    this.inputForm()
+  initialize() {
+    this.inputflag = false
+    this.inputLengthflag = false
+    this.emailflag = false
   }
 
   inputForm() {
@@ -29,28 +34,71 @@ export default class extends Controller {
 
     if(length > this.characterCountValue || length == 0) {
       this.inputLengthTarget.classList.add('text-danger')
-      this.formButtonTarget.disabled = true
+      this.inputLengthflag = false
     } else {
       this.inputLengthTarget.classList.remove('text-danger')
-      this.formButtonTarget.disabled = false
+      this.inputLengthflag = true
     }
   }
 
   allowedCharacters() {
     // 許可された文字チェック
-    const allowString = /^[a-zA-Z0-9０-９\u3040-\u309f\u30a0-\u30ff\uFF5E!\！\?\？\+\―\*\(\)\（\）\'\"\&\%\$\s。、ㇰヶㇱㇲㇳㇴㇵㇶㇷㇸㇹㇺャㇻㇼㇽㇾㇿヮ蟻好嬉喜友晴一運営]+$/
-    const exceptSpace = /\S+/
+    const allowRegex = /^[a-zA-Z0-9０-９\u3040-\u309f\u30a0-\u30ff\uFF5E!\！\?\？\+\―\*\(\)\（\）\'\"\&\%\$\s。、ㇰヶㇱㇲㇳㇴㇵㇶㇷㇸㇹㇺャㇻㇼㇽㇾㇿヮ蟻好嬉喜友晴一運営]+$/
+    const exceptSpaceRegex = /\S+/
     const inputValue = this.inputTarget.value
 
-    if(!inputValue.match(exceptSpace) || !inputValue) {
-      this.errorTarget.textContent = '空文字のみ、または入力されていません'
-      this.formButtonTarget.disabled = true
-    } else if(inputValue.match(allowString) && inputValue) {
-      this.errorTarget.textContent = ''
-      this.formButtonTarget.disabled = false
+    if(inputValue.match(allowRegex) && inputValue.match(exceptSpaceRegex) && inputValue) {
+      this.inputErrorTarget.textContent = ''
+      this.inputflag = true
+    } else if(!inputValue.match(exceptSpaceRegex) || !inputValue) {
+      this.inputErrorTarget.textContent = '空文字のみ、または入力されていません'
+      this.inputflag = false
     } else {
-      this.errorTarget.textContent = '使用できない文字が含まれています'
-      this.formButtonTarget.disabled = true
+      this.inputErrorTarget.textContent = '使用できない文字が含まれています'
+      this.inputflag = false
+    }
+  }
+
+  validateEmail() {
+    const validEmailRegex = /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/
+    const exceptSpaceRegex = /\S+/
+    const emailValue = this.emailTarget.value
+
+    if(emailValue.match(validEmailRegex) && emailValue) {
+      this.emailErrorTarget.textContent = ''
+      this.emailflag = true
+    } else if(!emailValue.match(exceptSpaceRegex) || !emailValue) {
+      this.emailErrorTarget.textContent = '空文字のみ、または入力されていません'
+      this.emailflag = false
+    } else {
+      this.emailErrorTarget.textContent = 'メールの形式ではありません'
+      this.emailflag = false
+    }
+  }
+
+  submitButtonChange() {
+    switch (this.formTypeValue) {
+      case 'userCreate':
+        if(this.inputflag && this.inputLengthflag && this.emailflag) {
+          this.submitButtonTarget.disabled = false
+        } else {
+          this.submitButtonTarget.disabled = true
+        }
+        break
+      case 'userSession':
+        if(this.emailflag) {
+          this.submitButtonTarget.disabled = false
+        } else {
+          this.submitButtonTarget.disabled = true
+        }
+        break
+      case 'tweetCreate':
+        if(this.inputflag && this.inputLengthflag) {
+          this.submitButtonTarget.disabled = false
+        } else {
+          this.submitButtonTarget.disabled = true
+        }
+        break
     }
   }
 }
