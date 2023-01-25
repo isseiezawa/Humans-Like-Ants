2d892e80658f246c78990f89995519a2b4702af2
issei_ezawa <issemann@yahoo.co.jp>
2023-01-26 07:10:07 +0900

    Add: twitter_idからプロフィール画面へ繋がるURL生成する処理追加

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,12 +1,7 @@
 class UserDecorator < ApplicationDecorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def to_twitter_url
+    twitter_id? ? "https://twitter.com/#{twitter_id}" : nil
+  end
 end
