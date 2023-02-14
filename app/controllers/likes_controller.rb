8cf83d4566b4d13c61cb3ee639c8bdb88812ea89
issei_ezawa <issemann@yahoo.co.jp>
2023-02-15 06:49:47 +0900

    Add: like button追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,20 @@ class LikesController < ApplicationController
   def create
     tweet = Tweet.find(params[:tweet_id])
     current_user.like(tweet)
+    render turbo_stream: turbo_stream.replace(
+      "like-button-#{tweet.id}",
+      partial: 'likes/like_button',
+      locals: { tweet: tweet, liked: true}
+    )
   end
 
   def destroy
     tweet = Tweet.find(params[:tweet_id])
     current_user.unlike(tweet)
+    render turbo_stream: turbo_stream.replace(
+      "like-button-#{tweet.id}",
+      partial: 'likes/like_button',
+      locals: { tweet: tweet, liked: false}
+    )
   end
 end
