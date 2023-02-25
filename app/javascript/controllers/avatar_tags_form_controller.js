def4ce6b8830c0a16f7c3ecdd180093f49031e99
issei_ezawa <issemann@yahoo.co.jp>
2023-02-25 16:10:12 +0900

    Add: tagの追加(新規作成)機能作成
    実際の作成機構はuser.rb記載のAvatarTag.find_or_create_by部分

--- app/javascript/controllers/avatar_tags_form_controller.js
+++ app/javascript/controllers/avatar_tags_form_controller.js
@@ -2,15 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="avatar-tags-form"
 export default class extends Controller {
-  static targets = ['avatarTag']
+  static targets = ['inputAvatarTag']
 
   connect() {
     this.tagsListElement = document.getElementById('avatar-tags-list')
     this.selectElement = document.getElementById('select-tag')
+
+    this.avatarTagList()
   }
 
   async avatarTagList() {
-    const params = this.avatarTagTarget.value
+    const params = this.inputAvatarTagTarget.value
     const response = await fetch(`${location.origin}/avatar_tags/search?name=${params}`)
     const response_json = await response.json()
 
@@ -29,13 +31,27 @@ export default class extends Controller {
 
   setAvatarTag(event) {
     const text = event.currentTarget.textContent
-    const textNode = document.createTextNode(text)
 
-    const id = new Date().getTime();
+    const id = new Date().getTime()
 
     const label_element = document.createElement('label')
     label_element.innerHTML = `<input type='checkbox' checked='checked' name='user[avatar_tags_attributes][${id}][name]' value='${text}'>${text}`
 
     this.tagsListElement.appendChild(label_element)
+  }
+
+  addAvatarTag() {
+    const input = this.inputAvatarTagTarget
+    const inputValue = input.value.replace(/\s+/g, '')
+    if(inputValue) {
+      const id = new Date().getTime()
+
+      const label_element = document.createElement('label')
+      label_element.innerHTML = `<input type='checkbox' checked='checked' name='user[avatar_tags_attributes][${id}][name]' value='${inputValue}'>${inputValue}`
+
+      this.tagsListElement.appendChild(label_element)
+
+      input.value = ''
+    }
   }
 }
