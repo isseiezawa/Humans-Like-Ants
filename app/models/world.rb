673d30c941b6c9eda433ef981984ef9434af390b
issei_ezawa <issemann@yahoo.co.jp>
2023-02-27 22:13:44 +0900

    Change: world index部分を部屋数カウントに変更

--- app/models/world.rb
+++ app/models/world.rb
@@ -5,6 +5,7 @@ class World < ActiveYaml::Base
   set_filename 'world'
 
   has_many :tweets # rubocop:disable Rails/HasManyOrHasOneDependent
+  has_many :world_rooms # rubocop:disable Rails/HasManyOrHasOneDependent
 
   def tweets_length(page_number)
     tweets.joins(:user).select(:post, :name).page(page_number).per(3).length
