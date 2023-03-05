d62ffaaa4572145345ba994c80bfc72724dda237
issei_ezawa <issemann@yahoo.co.jp>
2023-03-06 01:23:06 +0900

    Add: 未ログイン時にinformation index部分でrequire_loginが発動していたため修正

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,4 +1,6 @@
 class InformationController < ApplicationController
+  skip_before_action :require_login, only: %i[index]
+
   def index
     @informations = Information.includes(:user).with_attached_image.order(id: :desc).page(params[:page]).per(5)
   end
