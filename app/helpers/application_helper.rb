772adc8f7a5a4424ea77793f7a8ea4faec7404ba
issei_ezawa <issemann@yahoo.co.jp>
2023-03-05 16:27:09 +0900

    Add: default のmeta tag追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,30 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Humans Like Ants',
+      title: t('meta_tags.default_title'),
+      reverse: true, # タイトルタグ内の表記順をページタイトル|サイトタイトルの順にする
+      charset: 'utf-8',
+      description: t('meta_tags.description'),
+      keywords: 'metaverse, 3D, ants',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@issei423',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
