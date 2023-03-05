955e3ec5e81a562ceeadc99bc7c32dccba512b8b
issei_ezawa <issemann@yahoo.co.jp>
2023-03-05 19:05:51 +0900

    Add: admin::application_controllerの認証追加

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,10 +6,19 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    add_flash_types :success, :info, :warning, :danger
+    include Sorcery::Controller
+    before_action :require_login
     before_action :authenticate_admin
 
+    private
+
+    def not_authenticated
+      redirect_to login_path, warning: t('defaults.not_authenticated')
+    end
+
     def authenticate_admin
-      # TODO Add authentication logic here.
+      redirect_to root_path, warning: t('activerecord.errors.messages.not_found_admin') unless current_user.admin?
     end
 
     # Override this value to specify the number of elements to display at a time
