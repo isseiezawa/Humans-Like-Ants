9c53b40113b42c42caaec07d5bf2c05cbd67a6ae
issei_ezawa <issemann@yahoo.co.jp>
2023-03-06 22:51:52 +0900

    Add: プライバシーポリシー追加

--- config/routes.rb
+++ config/routes.rb
@@ -127,6 +127,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get 'guide', to: 'static_pages#guide'
   get 'terms_of_service', to: 'static_pages#terms_of_service'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
