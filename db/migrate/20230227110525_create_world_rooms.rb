953087fe6d10172dd7f49a3c17afb8a7abc962ee
issei_ezawa <issemann@yahoo.co.jp>
2023-06-06 18:09:19 +0900

    Fix: WorldRoomモデル作成時にuserと紐づけるように修正、不要なmigration fileの削除。

--- db/migrate/20230227110525_create_world_rooms.rb
+++ db/migrate/20230227110525_create_world_rooms.rb
@@ -3,6 +3,7 @@ class CreateWorldRooms < ActiveRecord::Migration[7.0]
     create_table :world_rooms do |t|
       t.integer :world_id, null: false
       t.string :name, null: false
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230227122939_insert_initial_world_rooms.rb
+++
@@ -1,9 +0,0 @@
-class InsertInitialWorldRooms < ActiveRecord::Migration[7.0]
-  def up
-    WorldRoom.create!(world_id: 13, name: 'Hello! Humans Like Ants!')
-  end
-
-  def down
-    WorldRoom.delete_all
-  end
-end
